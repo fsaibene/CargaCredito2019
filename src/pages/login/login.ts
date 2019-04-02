fbef6d31eedfa9bdebca570b294d9339a23ab192
Fernando <fernandomsaibene@hotmail.com>
2019-04-02 01:51:55 -0300

    Primer comm

---
+++ src/pages/login/login.ts
@@ -0,0 +1,77 @@
+import {Component} from "@angular/core";
+import {NavController, AlertController, ToastController, MenuController} from "ionic-angular";
+import {HomePage} from "../home/home";
+import {RegisterPage} from "../register/register";
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { User } from "../../model/user";
+import firebase from "firebase";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  public user = {} as User;
+
+  constructor(public nav: NavController,
+     public forgotCtrl: AlertController,
+      public menu: MenuController,
+      public afAuth: AngularFireAuth,
+      public toastCtrl: ToastController) {
+    this.menu.swipeEnable(false);
+  }
+
+  // go to register page
+  register() {
+    this.nav.setRoot(RegisterPage);
+  }
+
+  // login and go to home page
+  login(user: User) {
+    this.afAuth.auth.signInWithEmailAndPassword(user.mail, user.password)
+    .then(response => {
+      this.nav.setRoot(HomePage);
+    })
+    .catch(err => console.log(err));
+  }
+
+  forgotPass() {
+    let forgot = this.forgotCtrl.create({
+      title: 'Forgot Password?',
+      message: "Enter you email address to send a reset link password.",
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email',
+          type: 'email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Send',
+          handler: data => {
+            console.log('Send clicked');
+            let toast = this.toastCtrl.create({
+              message: 'Email was sended successfully',
+              duration: 3000,
+              position: 'top',
+              cssClass: 'dark-trans',
+              closeButtonText: 'OK',
+              showCloseButton: true
+            });
+            toast.present();
+          }
+        }
+      ]
+    });
+    forgot.present();
+  }
+
+}
